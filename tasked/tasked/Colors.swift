14c3780ea117a93901f53f2e986e1833e9c1d551
tiffany <tiffanylinnn21@gmail.com>
2022-03-07 21:58:14 +0800

    Add color theme

---
+++ tasked/tasked/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  tasked
+//
+//  Created by Tiffany Lin on 7/3/22.
+//
+//
+import SwiftUI
+
+extension Color {
+  static let yellowFill = Color(red: 1, green: 188 / 255.0, blue: 58 / 255.0)
+  static let yellowOutline = Color(red: 243 / 255.0, green: 161 / 255.0, blue: 36 / 255.0)
+  static let paleWhite = Color(white: 1, opacity: 179 / 255.0)
+  static let grey = Color(red: 229 / 255.0, green: 229 / 255.0, blue: 229 / 255.0)
+  static let darkgrey = Color(red: 85/255, green: 85/255, blue: 85/255)
+  static let locationRed = Color(red: 234/255, green: 95/255, blue: 104/255)
+  static let lightRed = Color(red: 255/255, green: 167/255, blue: 167/255, opacity: 0.5)
+  static let lightOrange = Color(.sRGB, red: 255/255, green: 214/255, blue: 167/255, opacity: 0.34)
+  static let wordColor = Color(.sRGB, red: 53/255, green: 32/255, blue: 63/255, opacity: 1)
+  static let lightWordColor = Color(.sRGB, red: 53/255, green: 32/255, blue: 63/255, opacity: 0.7)
+  static let yellowWordColor = Color(.sRGB, red: 243/255, green: 161/255, blue: 36/255)
+}
